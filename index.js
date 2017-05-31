71c7f2782b7e34692bc998efe6c2644c140022a1
rajbharath <rbrajbharath1@gmail.com>
2017-05-31 12:26:10 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var TOTAL_FILES = 7;
+
+app.use(express.static('./'));
+
+app.get('/audio', (req, res) => {
+   res.setHeader("content-type", "audio/mp3");
+   fs.createReadStream("./files/"+ getRandomInt() +".mp3").pipe(res);
+});
+
+function getRandomInt() {
+  return Math.floor(Math.random()*10) % TOTAL_FILES + 1
+}
+
+let portNumber = 5000;
+app.listen(portNumber, function () {
+  console.log('Goundamani Backend listening on port ' + portNumber);
+});
