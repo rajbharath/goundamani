8121a79caacd129afa56de2576f2af80342590d7
rajbharath <rbrajbharath1@gmail.com>
2017-05-31 14:58:32 +0530

    Raj | Working response

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ var fs = require('fs');
 var app = express();
 var TOTAL_FILES = 7;
 
-app.use(express.static('./'));
+app.use(express.static('files'));
 
-app.post('/audio', (req, res) => {
+app.post('/', (req, res) => {
+    console.log('Audio');
+    res.status(200).send(response);
+});
+
+app.post('/audio/:audioName', (req, res) => {
    res.setHeader("content-type", "audio/mp3");
    fs.createReadStream("./files/"+ getRandomInt() +".mp3").pipe(res);
 });
+
 
 function getRandomInt() {
   return Math.floor(Math.random()*10) % TOTAL_FILES + 1
@@ -18,3 +24,16 @@ let portNumber = 5000;
 app.listen(portNumber, function () {
   console.log('Goundamani Backend listening on port ' + portNumber);
 });
+
+
+var response = {
+  version: '1.0',
+  sessionAttributes: {},
+  response: {
+    outputSpeech: {
+      type: 'SSML',
+      ssml: '<speak><audio src=\"https://4b1ae59f.ngrok.io/1.mp3\" /></speak>'
+    }
+  }
+
+}
